5e3361287120a2788c7151901a9a202960feb64e
orbril <112857322+orbril@users.noreply.github.com>
2022-09-08 12:43:57 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+basic.showIcon(IconNames.Heart)
+for (let index = 0; index < 4; index++) {
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    0,
+    SuperBit.enMotors.M1,
+    0
+    )
+}
+basic.forever(function () {
+	
+})
